95561d70a52ffec19295fa7034a2f8773ec192d6
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2023-03-03 02:00:50 +0600

    Create solution.js
    34. Find First and Last Position of Element in Sorted Array
    
    Given an array of integers nums sorted in non-decreasing order, find the starting and 
    ending position of a given target value.
    
    If target is not found in the array, return [-1, -1].
    
    You must write an algorithm with O(log n) runtime complexity.
    
    Example 1:
    Input: nums = [5,7,7,8,8,10], target = 8
    Output: [3,4]
    
    Example 2:
    Input: nums = [5,7,7,8,8,10], target = 6
    Output: [-1,-1]
    Example 3:
    
    Input: nums = [], target = 0
    Output: [-1,-1]
     
    Constraints:
    0 <= nums.length <= 105
    -109 <= nums[i] <= 109
    nums is a non-decreasing array.
    -109 <= target <= 109

---
+++ Find First and Last Position of Element in Sorted Array/solution.js
@@ -0,0 +1,57 @@
+/*
+34. Find First and Last Position of Element in Sorted Array
+
+Given an array of integers nums sorted in non-decreasing order, find the starting and 
+ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+
+Example 2:
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+Constraints:
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+  let mySearchRange = [];
+  const lengthNums = nums.length;
+  if (lengthNums === 0 && target === 0) {
+    mySearchRange = [-1, -1];
+  }
+  for (i = 0; i <= lengthNums - 1; i++) {
+    if (nums[i] === target) {
+      mySearchRange.push(i);
+    }
+  }
+  const mySearchRangeLength = mySearchRange.length;
+  if (mySearchRangeLength === 0) {
+    mySearchRange = [-1, -1];
+  } else if (mySearchRangeLength === 1) {
+    mySearchRange.push(mySearchRange[0]);
+  } else if (mySearchRangeLength > 2) {
+    mySearchRange = [mySearchRange[0], mySearchRange[mySearchRangeLength - 1]];
+  }
+  return mySearchRange;
+};
+
+console.log(searchRange([5, 7, 7, 8, 8, 10], 6));
